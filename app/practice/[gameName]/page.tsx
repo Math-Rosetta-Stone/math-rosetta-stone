eecc3a02ebef5db243af7507518c480c80ba8526
Benjamin-Yu-Sheng-Chang <benji.chang@mail.utoronto.ca>
2026-01-02 14:17:01 -0500

    try to commit

--- app/practice/[gameName]/page.tsx
+++ app/practice/[gameName]/page.tsx
@@ -10,18 +10,18 @@ import { Game } from "@/types/map"
 
 export default function PracticeGame({ params }: { params: { gameName: Game } }): JSX.Element {
   switch (params.gameName) {
-    case "hangman":
-      return <Hangman />
-    case "mcq":
-      return <McqGame />
-    case "fib":
-      return <FibGame />
-    case "logo":
-      return <LogoGame />
-    case "listen":
-      return <ListeningGame />
-    case "matching":
-      return <MatchingGame />
+    // case "hangman":
+    //   return <Hangman />
+    // case "mcq":
+    //   return <McqGame />
+    // case "fib":
+    //   return <FibGame />
+    // case "logo":
+    //   return <LogoGame />
+    // case "listen":
+    //   return <ListeningGame />
+    // case "matching":
+    //   return <MatchingGame />
     default:
       return <div>Invalid Game</div>
   }
